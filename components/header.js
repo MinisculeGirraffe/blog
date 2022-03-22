60398b012b383b9bde2f87f4bdbdf68ef7635401
Daniel Norred <daniel@nor.red>
2022-03-22 13:14:43 -0500

    Change header, delete test posts

--- components/header.js
+++ components/header.js
@@ -5,7 +5,7 @@ export default function Header() {
     <nav className='flex  py-2 bg-stone-800/40 backdrop-blur-lg w-full sticky top-0 left-0 right-0 z-10 mb-7'>
       <div className="flex text-gray-200/97 font-bold text-4xl px-3 hover:underline hover:cursor-pointer">
         <Link href={"/"}>
-        <p>Daniel Norred</p>
+        <p>udp.lgbt</p>
         </Link>
 
       </div>
